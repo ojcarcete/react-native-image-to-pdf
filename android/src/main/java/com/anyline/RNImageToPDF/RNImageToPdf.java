400e8b8ad5ce2d0e6636781ca2d431296a7f82f1
Daniel Frenberg <frenberg@gmail.com>
2019-11-24 22:53:19 +0100

    android async pdf creation

--- android/src/main/java/com/anyline/RNImageToPDF/RNImageToPdf.java
+++ android/src/main/java/com/anyline/RNImageToPDF/RNImageToPdf.java
@@ -36,13 +36,10 @@ import static java.lang.String.format;
 public class RNImageToPdf extends ReactContextBaseJavaModule {
 
     public static final String REACT_CLASS = "RNImageToPdf";
-
-    private final ReactApplicationContext reactContext;
     private static final Logger log = Logger.getLogger(RNImageToPdf.REACT_CLASS);
 
     RNImageToPdf(ReactApplicationContext context) {
         super(context);
-        this.reactContext = context;
     }
 
     @Override
@@ -52,94 +49,7 @@ public class RNImageToPdf extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void createPDFbyImages(ReadableMap options, final Promise promise) {
-        ReadableArray images = options.getArray("imagePaths");
+        new CreatePDFAsyncTask(getReactApplicationContext(), promise, options).execute();
 
-        String documentName = options.getString("name");
-
-        ReadableMap maxSize = options.hasKey("maxSize") ? options.getMap("maxSize") : null;
-        int maxHeight = maxSize != null && maxSize.hasKey("height") ? maxSize.getInt("height") : 0;
-        int maxWidth = maxSize != null && maxSize.hasKey("width") ? maxSize.getInt("width") : 0;
-
-        int quality = options.hasKey("quality") ? (int)Math.round(100 * options.getDouble("quality")) : 0;
-
-        PdfDocument document = new PdfDocument();
-        try {
-
-            for (int idx = 0; idx < images.size(); idx++) {
-                // get image
-                Bitmap bmp = getImageFromFile(images.getString(idx));
-
-                // resize
-                bmp = resize(bmp, maxWidth, maxHeight);
-
-                // compress
-                bmp = compress(bmp, quality);
-
-                PageInfo pageInfo = new Builder(bmp.getWidth(), bmp.getHeight(), 1).create();
-
-                // start a page
-                Page page = document.startPage(pageInfo);
-
-                // add image to page
-                Canvas canvas = page.getCanvas();
-                canvas.drawBitmap(bmp, 0, 0, null);
-
-                document.finishPage(page);
-            }
-
-            // write the document content
-            File targetPath = reactContext.getExternalFilesDir(null);
-            File filePath = new File(targetPath, documentName);
-            document.writeTo(new FileOutputStream(filePath));
-            log.info(format("Wrote %,d bytes to %s", filePath.length(), filePath.getPath()));
-            WritableMap resultMap = Arguments.createMap();
-            resultMap.putString("filePath", filePath.getAbsolutePath());
-            promise.resolve(resultMap);
-        } catch (Exception e) {
-            promise.reject("failed", e);
-        }
-
-        // close the document
-        document.close();
     }
-
-    private Bitmap getImageFromFile(String path) throws IOException {
-        if (path.startsWith("content://")) {
-            return getImageFromContentResolver(path);
-        }
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        return BitmapFactory.decodeFile(path, options);
-    }
-
-    private Bitmap getImageFromContentResolver(String path) throws IOException {
-        ParcelFileDescriptor parcelFileDescriptor = reactContext.getContentResolver().openFileDescriptor(Uri.parse(path), "r");
-        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-        parcelFileDescriptor.close();
-        return image;
-    }
-
-    private Bitmap resize(Bitmap bitmap, int maxWidth, int maxHeight) {
-        if (maxWidth == 0 || maxHeight == 0) return bitmap;
-        if (bitmap.getWidth() <= maxWidth && bitmap.getHeight() <= maxHeight) return bitmap;
-
-        double aspectRatio = (double) bitmap.getHeight() / bitmap.getWidth();
-        int height = Math.round(maxWidth * aspectRatio) < maxHeight ? (int) Math.round(maxWidth * aspectRatio) : maxHeight;
-        int width = (int) Math.round(height / aspectRatio);
-
-        return Bitmap.createScaledBitmap(bitmap, width, height, true);
-    }
-
-    private Bitmap compress(Bitmap bmp, int quality) throws IOException {
-        if (quality <= 0 || quality >= 100) return bmp;
-
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, quality, stream);
-        byte[] byteArray = stream.toByteArray();
-        stream.close();
-        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-    }
-
 }
